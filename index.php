5292a0bca92e1d94c96b3549880f9d481bb9f9a1
Jeremy Dee <jeremydee@hotmail.com>
2012-05-24 09:09:56 -0500

    Fourth

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php echo "Hello Liz, we invite you to MKEPUG September 11th - Jeremy"; ?>
+<?php echo "Hello Liz, we invite you to <a href='http://www.mkepug.org'>MKEPUG</a> September 11th - Jeremy"; ?>
