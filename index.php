c15857d660f72ffe6273b111c6f613bb81aa634c
deej <deej@vostro>
2012-09-17 14:15:39 -0600

    Testing too see if this is still wired up!

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<?php echo "Hello Liz, we invite you to <a href='http://www.mkepug.org'>MKEPUG</a> September 11th - Jeremy"; ?>
+<?php echo "Hello Liz, we invite you to <a href='http://www.mkepug.org'>MKEPUG</a> September 11th - Jeremy - Hey it works!"; ?>
 
