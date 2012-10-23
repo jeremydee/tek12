395dc70154dbde88c098442ebf207b701485a834
deej <deej@vostro>
2012-10-23 14:12:28 -0500

    Messing around

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<?php echo "Hello Liz, we invite you to <a href='http://www.mkepug.org'>MKEPUG</a> September 11th - Jeremy - Hey it works!"; ?>
+<?php echo "Hello Liz, we invite you to <a href='http://www.mkepug.org'>MKEPUG</a> September 11th - Jeremy - Hey it works! Even in October"; ?>
 
