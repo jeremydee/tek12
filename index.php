ee344e51e1028228c7be20f71de4727cd731c253
deej <deej@vostro>
2012-10-23 14:17:15 -0500

    Messing around

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<?php echo "Hello Liz, we invite you to <a href='http://www.mkepug.org'>MKEPUG</a> September 11th - Jeremy - Hey it works! Even in October"; ?>
+<?php echo "Test to see how fast updates to orchestra are"; ?>
 
